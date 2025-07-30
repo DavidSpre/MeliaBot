3ad42bea518859d016948ff2d1e1f3330f428238
David Sp <david.sprengart@web.de>
2025-07-30 22:09:10 +0200

    fix bug in isAdmin

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,12 @@
 // check if user has admin rights in the current server/channel
 const isAdmin = function(msg) {
-    return msg.member.permissionsIn(msg.channel).has("ADMINISTRATOR");
+    try {
+        // user is admin - pass
+        return msg.member.permissionsIn(msg.channel).has("ADMINISTRATOR");
+    } catch(err) {
+        // user is not an admin - return
+        return false;
+    }
 };
 
 module.exports = { isAdmin };
