9bc0880ae7b827b0fb11a7994ba94dd482083c94
David Sp <david.sprengart@web.de>
2025-07-31 21:03:23 +0200

    add generateRandomInteger function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,9 +4,18 @@ const isAdmin = function(msg) {
         // user is admin - pass
         return msg.member.permissionsIn(msg.channel).has("ADMINISTRATOR");
     } catch(err) {
+        // TODO: Check if error can be prevented by adjusting Melia's permissions
         // user is not an admin - return
         return false;
     }
 };
 
-module.exports = { isAdmin };
+// generates a random integer between min and max
+const generateRandomInteger = function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+module.exports = { isAdmin, generateRandomInteger };
